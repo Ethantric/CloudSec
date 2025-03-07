56330c620125a26e9199c3ac392caeee725708b2
Ethantric <comiskeye@gmail.com>
2025-03-07 15:13:09 -0600

    Create AWS S3 bucket and upload file
    This program creates an AWS S3 bucket and gives the user the option to upload a file to that bucket or another existing bucket in their account

--- App.java
+++ App.java
@@ -1,16 +1,17 @@
-package com.example.assignment1_4590;
-
-import com.amazonaws.AmazonClientException;
+package com.example.assignment1_2;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.services.ec2.AmazonEC2;
-import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
-import com.amazonaws.services.ec2.model.RunInstancesRequest;
-import com.amazonaws.services.ec2.model.RunInstancesResult;
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.amazonaws.services.s3.model.GetBucketLocationRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import java.io.File;
 import java.util.Scanner;
-
-
 
 public class App {
     public static void main(String[] args) {
@@ -24,30 +25,83 @@ public class App {
         			"Please make sure that your credentials file is at the correct " +
         			"location (C:/Users/%USERPROFILE%/.aws/credentials), and is in valid format.", e);
         }
-        
-    	//Ask user parameters
-    	Scanner scanner = new Scanner(System.in);
-    	System.out.println("\nThe following is a series of parameters needed to create an EC2 instance:");
-    	System.out.print("Region should the instance be created in: ");
-    	String region = scanner.nextLine();
-    	System.out.print("Name of the security group: ");
-    	String secGroup = scanner.nextLine();
-    	System.out.print("Name of the instance type: ");
-    	String insType = scanner.nextLine();
-    	System.out.print("What is the image ID: ");
-    	String imageID = scanner.nextLine();
-    	System.out.print("Name of the key pair for login: ");
-    	String keyName = scanner.nextLine();
-    	scanner.close();
-    	
-        //Create EC2 client
-        AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(region).build();
-        
-        //Create request to EC2 client
-        RunInstancesRequest runInstancesRequest = new RunInstancesRequest();
-        runInstancesRequest.withImageId(imageID).withInstanceType(insType).withMinCount(1).withMaxCount(1).withKeyName(keyName).withSecurityGroups(secGroup);
-        RunInstancesResult result = ec2.runInstances(runInstancesRequest);
-        System.out.println("EC2 Instance created!");
-        ec2.shutdown();
+        Scanner scanner = new Scanner(System.in);
+        int userChoice = 0;
+        while(userChoice != 3) {
+        	//Give user choice on creating bucket, uploading file to bucket, or ending the program 
+        	System.out.print("Enter 1 to create a new bucket, "
+        			+ "2 to add a file to an existing bucket, "
+        			+ "or 3 to end the program: ");
+        	userChoice = scanner.nextInt();
+        	scanner.nextLine();
+        	
+        	if(userChoice == 1) {
+        		//Obtain information to create bucket
+        		System.out.print("The following information is needed to create an S3 bucket");
+                System.out.print("\nWhat region do you want this bucket to be located in?: ");
+                System.out.flush();
+                String region = scanner.nextLine();
+                System.out.print("What is the name of the bucket? (Must be unique): ");
+                System.out.flush();
+                String bucketName = scanner.nextLine();
+                //Attempt to create s3 bucket
+                try {
+                    AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                            .withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(region).build();
+                    if (!s3Client.doesBucketExistV2(bucketName)) {
+                        s3Client.createBucket(new CreateBucketRequest(bucketName));
+                        String bucketLocation = s3Client.getBucketLocation(new GetBucketLocationRequest(bucketName));
+                        System.out.println("Bucket location: " + bucketLocation);
+                    }
+                    s3Client.shutdown();
+                //In case exceptions are thrown during creation
+                } catch (AmazonServiceException e) {
+                    e.printStackTrace();
+                } catch (SdkClientException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Bucket created with name: " + bucketName + "!");
+        	}else if(userChoice == 2) {
+        		//Obtain information about existing bucket and file info
+        		System.out.print("Enter the name of the S3 bucket: ");
+        	    System.out.flush();
+        	    String bucketName = scanner.nextLine();
+        	    System.out.print("Please state the region the bucket is located in: ");
+        	    String region = scanner.nextLine();
+        	    System.out.print("Enter the full path of the object you would like to upload: ");
+        	    System.out.flush();
+        	    String filePath = scanner.nextLine();
+        	    File file = new File(filePath);
+        	    //Check if file exists
+        	    if (!file.exists()) {
+        	        System.out.println("Error: File does not exist. Please check the path.");
+        	    } else {
+        	    	//Attempt to upload file
+        	        try {
+        	            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+        	                    .withCredentials(new AWSStaticCredentialsProvider(credentials))
+        	                    .withRegion(region)
+        	                    .build();
+        	            String objectKey = file.getName();
+        	            System.out.println("Uploading file...");
+        	            s3Client.putObject(new PutObjectRequest(bucketName, objectKey, file));
+        	            System.out.println("File uploaded successfully to bucket: " + bucketName);
+        	            s3Client.shutdown();
+        	        //Catch any exceptions
+        	        } catch (AmazonServiceException e) {
+        	            e.printStackTrace();
+        	        } catch (SdkClientException e) {
+        	            e.printStackTrace();
+        	        }
+        	    }
+        	//User wants to exit program
+        	}else if (userChoice == 3){
+        		break;
+        	//User enters invalid option
+        	}else {
+        		System.out.println("Invalid option. Please enter 1, 2, or 3");
+        	}
+        }
+        scanner.close();
     }
 }
